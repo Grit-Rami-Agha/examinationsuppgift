621113aca3fb25fe1e43f8320013f947843d3a64
Rami-Agha <142787213+Rami-Agha@users.noreply.github.com>
2024-10-23 10:15:23 +0200

    .

--- javascript.js
+++ javascript.js
@@ -11,3 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('.nav-list li a');
+
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active');
+        }
+    });
+});
